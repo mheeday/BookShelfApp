b47ea3d42c98962e429a5fb2a6b6cb1642da0bc5
mheeday <66279843+mheeday@users.noreply.github.com>
2022-04-28 22:53:14 +0100

    Home Page Update

--- Django/libsproject/mainlib/views.py
+++ Django/libsproject/mainlib/views.py
@@ -2,13 +2,32 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from django.contrib import messages
 from . import forms
+from .models import Books
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'mainlib/home.html')
+    cat_options = {'CO': 'COMIC',
+        'FA': 'FANTASY',
+        'AC': 'ACTION',
+        'TH': 'THRILLER',
+        'CN': 'CONTEMPORARY',
+    }
+
+    context = {'context_home':[]}
+
+    for i in range(3):
+        a = random.choices(list(cat_options.keys()))[0]
+        b = random.choice(Books.objects.filter(book_cat=a))
+        context['context_home'].append(cat_options[a])
+        context['context_home'].append(b)
+        context['context_home'].append(f"mainlib/{b.book_cover}.jpg")
+        cat_options.pop(a)
+    print(context)
+    return render(request, 'mainlib/home.html', context)
 
 """ @pass
 def user_home(request, username):
