e6cebd839ed1eeac731b73edea30fe6b3fa116ac
mheeday <66279843+mheeday@users.noreply.github.com>
2022-04-29 16:53:47 +0100

    SIngle Book Page

--- Django/libsproject/mainlib/urls.py
+++ Django/libsproject/mainlib/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('a/', user_views.home, name='home'),
     path('register/', user_views.register, name='register'),
     path('login/', user_views.login, name='login'),
-    path('logout/', user_views.logout, name='logout')
+    path('logout/', user_views.logout, name='logout'),
+    path('<str:username>/home/', user_views.user_home, name='user_home'),
+    path('books/<str:category>/', user_views.book_cat_list, name='book_cat_list'),
     #path('<int:question_id>/', views.details, name='detail'),
 
 ]
--- Django/libsproject/mainlib/views.py
+++ Django/libsproject/mainlib/views.py
@@ -2,44 +2,79 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from django.contrib import messages
 from . import forms
-from .models import Books
+from .models import Books, UserBooks
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.http import HttpResponse
 import random
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
-def home(request):
-    cat_options = {'CO': 'COMIC',
-        'FA': 'FANTASY',
-        'AC': 'ACTION',
-        'TH': 'THRILLER',
-        'CN': 'CONTEMPORARY',
+cate_options = {'Comic': 'CO',
+                'Fantasy': 'FA',
+                'Action': 'AC',
+                'Thriller': 'TH',
+                'Contemporary': 'CN',
     }
 
+def get_random_books(each_cat):
+    cat_options = cate_options.copy()
     context = {'context_home':[]}
 
     for i in range(3):
-        a = random.choices(list(cat_options.keys()))[0]
-        b = random.choice(Books.objects.filter(book_cat=a))
-        context['context_home'].append(cat_options[a])
-        context['context_home'].append(b)
-        context['context_home'].append(f"mainlib/{b.book_cover}.jpg")
-        cat_options.pop(a)
-    print(context)
+        key = random.choices(list(cat_options.keys()))[0]
+        value = cat_options[key]
+        context['context_home'].append(key)
+        books_to_choose = Books.objects.filter(book_cat=value)
+        for j in range(each_cat):
+            b = random.choice(books_to_choose)
+            book_code = b.book_cover
+            context['context_home'].append(b)
+            context['context_home'].append(f"mainlib/{book_code}.jpg")
+            books_to_choose = books_to_choose.exclude(book_cover=book_code)
+        cat_options.pop(key)
+
+    return context
+
+
+def home(request):
+    context = get_random_books(1)
+    if request.user.is_authenticated:
+        return redirect('user_home', username=request.user.username)
     return render(request, 'mainlib/home.html', context)
 
-""" @pass
+@login_required
 def user_home(request, username):
     if request.user.is_authenticated:
+        if UserBooks.objects.filter(user=request.user).exists():
+            context = get_random_books(3)
+            return render(request, 'mainlib/user_home.html', context)
+        else:
+            context = get_random_books(3)
+            return render(request, 'mainlib/user_home.html', context)
+
+            
+
         #get users last viewed three genre
         #get users last viewed books there
-        return render(request, )
+            return render(request, 'mainlib/user_home.html', context)
     else:
         messages.info(request, f"Please, Login.")
-        return redirect('login') """
+        return redirect('login')
 
+def book_cat_list(request, category):
 
+    context = {'context_text':[]}
+    category = category.capitalize()
+    cat_value = cate_options[category]
+
+    books = Books.objects.filter(book_cat=cat_value)
+
+    for book in books:
+        context['context_text'].append(book)
+        #img_link = f"mainlib/{book.book_cover}.jpg"
+        #context['context_text'].append(img_link)
+
+    return render(request, 'mainlib/book_cat_list.html', context)
 
 
 def register(request):
@@ -68,7 +103,8 @@ def login(request):
             if user is not None:
                 auth_login(request, user)
                 messages.info(request, f"Welcome back, {username}.")
-                return redirect('home')
+                print(f" Current User ||||||||| {request.user.email}")
+                return redirect('user_home', username)
             else:
                 messages.warning(request, "Invalid Username or Password!")
         else:
